13a3fafddf57ff061187b8fd1edf1208510b723c
bibablloba <vladvaledol@gmail.com>
2024-12-09 21:11:15 +0300

    WIP

--- backend/main.py
+++ backend/main.py
@@ -3,7 +3,7 @@ from typing import Annotated
 from authx import AuthX, AuthXConfig
 from fastapi import Depends, FastAPI, HTTPException, Response, status
 from pydantic import BaseModel
-from sqlalchemy.ext.asyncio.session import AsyncSession
+from sqlalchemy.ext.asyncio.session import AsyncSession, async_sessionmaker
 
 # import auth
 from database import *
@@ -11,7 +11,12 @@ from models import *
 
 app = FastAPI()
 
+async def create_async_session():
+    async with async_session_maker()
 
 @app.post("/tables")
 async def create_tables():
-    Base.metadata.create_all(async_engine)
+    async with async_engine.begin() as conn:
+        await conn.run_sync(Base.metadata.drop_all)
+        await conn.run_sync(Base.metadata.create_all)
+    return {"ok": True}
--- backend/models.py
+++ backend/models.py
@@ -1,6 +1,7 @@
-from fastapi_users.db import SQLAlchemyBaseUserTable
+from fastapi_users.db import SQLAlchemyBaseUserTable, SQLAlchemyUserDatabase
 from sqlalchemy import Column, Integer, String
-from sqlalchemy.orm import DeclarativeBase
+from sqlalchemy.ext.asyncio import AsyncSession
+from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column
 
 
 class Base(DeclarativeBase):
@@ -9,3 +10,9 @@ class Base(DeclarativeBase):
 
 class UserModel(Base, SQLAlchemyBaseUserTable[int]):
     __tablename__ = "users"
+
+    Id: Mapped[int] = mapped_column(primary_key=True)
+
+    @classmethod
+    def get_db(cls, session: "AsyncSession"):
+        return SQLAlchemyUserDatabase(session, UserModel)
