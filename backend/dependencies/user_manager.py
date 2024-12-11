b9261000fc8604e79057d298d761f6a064cd3180
bibablloba <vladvaledol@gmail.com>
2024-12-11 17:50:50 +0300

    user schema

--- backend/dependencies/user_manager.py
+++ backend/dependencies/user_manager.py
@@ -1,5 +1,9 @@
 from fastapi import Depends
 
+from user_manager import UserManager
 
-async def get_user_manager(user_db=Depends(get_user_db)):
-    yield UserManager(user_db)
+from .users import get_user_db
+
+
+async def get_user_manager(users_db=Depends(get_user_db)):
+    yield UserManager(users_db)
