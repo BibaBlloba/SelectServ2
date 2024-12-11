069ef492315a94bc6194cf16507832ffca863b92
bibablloba <vladvaledol@gmail.com>
2024-12-11 17:11:47 +0300

    create strategy dependency

--- backend/dependencies/strategy.py
+++ backend/dependencies/strategy.py
@@ -10,5 +10,5 @@ from models import AccessToken
 
 def get_database_strategy(
     access_token_db: AccessTokenDatabase[AccessToken] = Depends(get_access_token_db),
-):
+) -> DatabaseStrategy:
     return DatabaseStrategy(access_token_db, lifetime_seconds=3600)
