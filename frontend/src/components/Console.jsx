a3e37ebbd17aeadd160660b25258a146691a603e
bibablloba <vladvaledol@gmail.com>
2024-12-12 23:54:13 +0300

    errors

--- frontend/src/components/Console.jsx
+++ frontend/src/components/Console.jsx
@@ -1,8 +1,37 @@
+import { AppProvider } from '@toolpad/core'
 import React from 'react'
+import { MdDashboard } from 'react-icons/md';
+import { DashboardLayout } from '@toolpad/core/DashboardLayout';
+
+const NAVIGATION = [
+  {
+    kind: 'header',
+    title: 'Main items',
+  },
+  {
+    segment: 'page',
+    title: 'Page',
+    icon: <MdDashboard />,
+  },
+  {
+    segment: 'page-2',
+    title: 'Page 2',
+  },
+];
 
 const Console = () => {
   return (
-    <div>Console</div>
+    <div>
+      <AppProvider navigation={NAVIGATION}
+      // router={router}
+      // theme={demoTheme}
+      // window={demoWindow}
+      >
+        <DashboardLayout>
+          <div>aa</div>
+        </DashboardLayout>
+      </AppProvider>
+    </div >
   )
 }
 
