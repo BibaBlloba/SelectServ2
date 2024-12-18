12e13da747f2552f5e3100807e1bd8241e19876d
bibablloba <vladvaledol@gmail.com>
2024-12-18 18:51:33 +0300

    Master Register Menu

--- frontend/src/components/MasterRegister.jsx
+++ frontend/src/components/MasterRegister.jsx
@@ -1,4 +1,4 @@
-import { Button, Checkbox, Divider, Form, Input } from "antd";
+import { Anchor, Button, Checkbox, Divider, Form, Input } from "antd";
 import { useRef, useState, useEffect } from "react";
 import { FaGoogle, FaSteam, FaYandex } from "react-icons/fa";
 import Register from "./Register";
@@ -40,7 +40,7 @@ const MasterRegister = () => {
 
   return (
     <div className="bg-white p-5 hidden sm:block">
-      <div className="max-h-[670px] bg-[url(./assets/MasterRegister.jpg)] bg-cover bg-right rounded-2xl flex flex-row p-12 justify-between">
+      <div className="max-h-[700px] bg-[url(./assets/MasterRegister.jpg)] bg-cover bg-right rounded-2xl flex flex-row p-12 justify-between">
         <div className="flex flex-col justify-between ">
           <h1 className="text-white text-5xl max-w-[200px]">
             Начните пользоваться продуктами сейчас
@@ -51,7 +51,22 @@ const MasterRegister = () => {
             <p>support@selectel.ru</p>
           </div>
         </div>
-        <div className="bg-white rounded-xl border-[5px] border-solid border-gray-300 p-8 w-[430px] h-[590px]">
+        <div className="bg-white rounded-xl border-[5px] border-solid border-gray-300 p-8 w-[430px] h-[610px] flex flex-col gap-4">
+          <Anchor
+            direction="horizontal"
+            items={[
+              {
+                key: 'Register',
+                href: '/MasterRegister',
+                title: 'Регистрация',
+              },
+              {
+                key: 'Login',
+                href: '/MasterLogin',
+                title: 'Логин',
+              },
+            ]}
+          />
           <Outlet />
         </div>
       </div>
