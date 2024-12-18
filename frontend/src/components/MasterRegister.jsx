4f05e49ec5dd6a42978bf3e929e767ad78a8e9c4
bibablloba <vladvaledol@gmail.com>
2024-12-18 18:34:23 +0300

    init

--- frontend/src/components/MasterRegister.jsx
+++ frontend/src/components/MasterRegister.jsx
@@ -22,6 +22,20 @@ const MasterRegister = () => {
     console.log("Failed:", errorInfo);
   };
 
+  const items = [
+    {
+      key: "/masterRegister",
+      label: "Основные",
+      // icon: <IoPerson size={20} />
+    },
+    {
+      key: "/profile/nav2",
+      label: "Платежная информация",
+      // icon: <FaMoneyBill size={20} />
+    },
+  ]
+
+
   return (
     <div className="bg-white p-5 hidden sm:block">
       <div className="max-h-[670px] bg-[url(./assets/MasterRegister.jpg)] bg-cover bg-right rounded-2xl flex flex-row p-12 justify-between">
@@ -35,86 +49,7 @@ const MasterRegister = () => {
             <p>support@selectel.ru</p>
           </div>
         </div>
-        <div className="bg-white rounded-xl border-[5px] border-solid border-gray-300 p-8">
-          <Form
-            layout="vertical"
-            initialValues={{
-              remember: true,
-            }}
-            onFinish={onFinish}
-            onFinishFailed={onFinishFailed}
-          >
-            <Form.Item
-              label="Электронная почта"
-              name="email"
-              rules={[
-                {
-                  required: true,
-                  message: "Пожайлуста, введите почту",
-                },
-              ]}
-            >
-              <Input />
-            </Form.Item>
-            <Form.Item
-              label="Пароль"
-              name="passwd"
-              rules={[
-                {
-                  required: true,
-                  message: "Пожайлуста, введите пароль",
-                },
-              ]}
-            >
-              <Input />
-            </Form.Item>
-            <Form.Item
-              label="Повторите пароль"
-              name="passwdRepeat"
-              rules={[
-                {
-                  required: true,
-                  message: "Пожайлуста, повторите пароль",
-                },
-              ]}
-            >
-              <Input />
-            </Form.Item>
-
-            <Form.Item name="newsCheck" valuePropName="newsCheck" label={null}>
-              <Checkbox>Я хочу получать новостную рассылку</Checkbox>
-            </Form.Item>
-            <Form.Item
-              name="licenseCheck"
-              valuePropName="licenseCheck"
-              label={null}
-              rules={[
-                {
-                  required: false,
-                  message: "Обязательно для регистрации",
-                },
-              ]}
-            >
-              <Checkbox>Соглашение на обработку персональных данных</Checkbox>
-            </Form.Item>
-
-            <Form.Item label={null}>
-              <Button
-                variant="contained"
-                danger
-                type="primary"
-                htmlType="submit"
-              >
-                Подтвердить
-              </Button>
-            </Form.Item>
-            <Divider >or Login with</Divider>
-            <div className="flex flex-row justify-start gap-5">
-              <Button><FaGoogle style={{ fontSize: 18 }} /></Button>
-              <Button><FaSteam style={{ fontSize: 18 }} /></Button>
-              <Button><FaYandex style={{ fontSize: 18 }} /></Button>
-            </div>
-          </Form>
+        <div className="bg-white rounded-xl border-[5px] border-solid border-gray-300 p-8 w-[430px] h-[590px]">
         </div>
       </div>
     </div>
---
+++ frontend/src/components/Register.jsx
@@ -0,0 +1,102 @@
+import { Button, Checkbox, Divider, Form, Input } from "antd";
+import { useRef, useState, useEffect } from "react";
+import { FaGoogle, FaSteam, FaYandex } from "react-icons/fa";
+import React from 'react'
+
+const Register = () => {
+  const [loing, setLogin] = useState('');
+  const [password, setPassword] = useState('');
+
+  const onFinish = (values) => {
+    console.log("Success:", values.email);
+    setLogin(values.email);
+    setPassword(values.password);
+  };
+
+  const onFinishFailed = (errorInfo) => {
+    console.log("Failed:", errorInfo);
+  };
+  return (
+    <Form
+      layout="vertical"
+      initialValues={{
+        remember: true,
+      }}
+      onFinish={onFinish}
+      onFinishFailed={onFinishFailed}
+    >
+      <Form.Item
+        label="Электронная почта"
+        name="email"
+        rules={[
+          {
+            required: true,
+            message: "Пожайлуста, введите почту",
+          },
+        ]}
+      >
+        <Input />
+      </Form.Item>
+      <Form.Item
+        label="Пароль"
+        name="passwd"
+        rules={[
+          {
+            required: true,
+            message: "Пожайлуста, введите пароль",
+          },
+        ]}
+      >
+        <Input />
+      </Form.Item>
+      <Form.Item
+        label="Повторите пароль"
+        name="passwdRepeat"
+        rules={[
+          {
+            required: true,
+            message: "Пожайлуста, повторите пароль",
+          },
+        ]}
+      >
+        <Input />
+      </Form.Item>
+
+      <Form.Item name="newsCheck" valuePropName="newsCheck" label={null}>
+        <Checkbox>Я хочу получать новостную рассылку</Checkbox>
+      </Form.Item>
+      <Form.Item
+        name="licenseCheck"
+        valuePropName="licenseCheck"
+        label={null}
+        rules={[
+          {
+            required: false,
+            message: "Обязательно для регистрации",
+          },
+        ]}
+      >
+        <Checkbox>Соглашение на обработку персональных данных</Checkbox>
+      </Form.Item>
+
+      <Form.Item label={null}>
+        <Button
+          variant="contained"
+          danger
+          type="primary"
+          htmlType="submit"
+        >
+          Подтвердить
+        </Button>
+      </Form.Item>
+      <Divider >or Login with</Divider>
+      <div className="flex flex-row justify-start gap-5">
+        <Button><FaGoogle style={{ fontSize: 18 }} /></Button>
+        <Button><FaSteam style={{ fontSize: 18 }} /></Button>
+        <Button><FaYandex style={{ fontSize: 18 }} /></Button>
+      </div>
+    </Form>
+  )
+}
+
+export default Register
