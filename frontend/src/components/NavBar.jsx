e8a161ef28cb7a4e951d89275a69f3ccd570897c
Akeka_ <vladvaledol@gmail.com>
2025-01-20 22:50:53 +0300

    Logout button

--- frontend/src/components/NavBar.jsx
+++ frontend/src/components/NavBar.jsx
@@ -1,8 +1,17 @@
 import { navLinks } from "../constants";
 import { Button } from "antd";
 import MobileDrawer from "./MobileDrawer";
+import { useContext } from "react";
+import { UserContext } from "../context/UserContext";
 
 function NavBar() {
+
+  const [token, setToken] = useContext(UserContext)
+
+  const handleLogout = () => {
+    setToken(null)
+  }
+
   return (
     <nav className="z-20 w-full flex py-4 justify-between items-center sm:px-10  px-6 sticky top-0 text-white bg-gradient-to-r to-black from-[#701E1E]">
       <a href="/"><h1 className="text-xl sm:text-2xl  font-medium">Select Serv</h1></a>
@@ -30,6 +39,16 @@ function NavBar() {
           </Button>
         </a>
         <MobileDrawer />
+        {token && (
+          <Button
+            variant="outlined"
+            className="sm:flex hidden"
+            danger
+            onClick={handleLogout}
+          >
+            Выход
+          </Button>
+        )}
       </div>
     </nav>
   );
