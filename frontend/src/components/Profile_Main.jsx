ae5ade68fead0b81243e614a57b918d98825da20
Akeka_ <vladvaledol@gmail.com>
2025-01-21 18:48:05 +0300

    Profile Main add email

--- frontend/src/components/Profile_Main.jsx
+++ frontend/src/components/Profile_Main.jsx
@@ -1,6 +1,28 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 
 const Profile_Main = () => {
+
+  const [token] = useState(localStorage.getItem("UserToken"))
+  const [email, setEmail] = useState("")
+  const [data, setData] = useState(null)
+
+  useEffect(() => {
+    const fetchUser = async () => {
+      const requestOptions = {
+        method: "GET",
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: "Bearer " + token,
+        }
+      }
+      const response = await fetch("http://localhost:8000/users/me", requestOptions)
+      const data = await response.json()
+      setData(data)
+    }
+
+    fetchUser();
+  })
+
   return (
     <div className='flex flex-col gap-6'>
       <h1 className='text-3xl'>Профиль</h1>
@@ -13,10 +35,12 @@ const Profile_Main = () => {
           <p>Роль</p>
         </div>
         <div className='flex flex-col gap-6'>
-          <p>ФИО</p>
-          <p>ПОЧТА</p>
-          <p>ID</p>
-          <p>Роль</p>
+          <p>Нет данных</p>
+          {data ? (
+            <p>{data.email}</p>
+          ) : <p>Нет данных</p>}
+          <p>Нет данных</p>
+          <p>Нет данных</p>
         </div>
       </div>
     </div>
