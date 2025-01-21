5ae46003f28413e9ad45b3d2bc27d7348e8dec59
Akeka_ <vladvaledol@gmail.com>
2025-01-21 19:32:46 +0300

    Button dark fix

--- frontend/src/components/Button_dark.jsx
+++ frontend/src/components/Button_dark.jsx
@@ -1,11 +1,11 @@
 import React from 'react'
 
-const Button_dark = ({ className, onClick, href, text, onChange }) => {
+const Button_dark = ({ className, onClick, href, text }) => {
   return (
     <>
       <button className='border-solid border-[1px] rounded-md text-gray-300 border-gray-300 w-[100px] h-[35px]
         transition-colors delay-100 hover:border-red-600 hover:text-red-600  active:bg-red-900'
-        onChange={onChange}>{text}</button>
+        onClick={onClick}>{text}</button>
     </>
   )
 }
