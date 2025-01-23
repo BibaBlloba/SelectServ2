d13bebd2c4ee37758d2a83b318e7daaf7fc2950f
Akeka_ <vladvaledol@gmail.com>
2025-01-24 00:08:42 +0300

    Sponsors links

--- frontend/src/components/Sponsor.jsx
+++ frontend/src/components/Sponsor.jsx
@@ -5,19 +5,19 @@ const Sponsor = () => {
   return (
     <div className="overflow-hidden whitespace-nowrap bg-white">
       <div className="logos-slider inline-block space-x-[200px] p-[30px] animate-loop-scroll">
-        <img loading="lazy" src={mrbeast} />
-        <img loading="lazy" src={forbes} />
-        <img loading="lazy" src="https://upload.wikimedia.org/wikipedia/commons/8/85/BlackRock_wordmark.svg" />
+        <a href="https://youtu.be/raGL4DIh40s?si=ESJ61nwuLuqX71-f" target="_blank"><img loading="lazy" src={mrbeast} /></a>
+        <a href="https://www.forbes.com/billionaires/" target="_blank"><img loading="lazy" src={forbes} /></a>
+        <a href="https://www.reddit.com/r/GGdiscussion/comments/1g06rt7/can_someone_explain_to_me_what_blackrocks_evil/" target="_blank"><img loading="lazy" src="https://upload.wikimedia.org/wikipedia/commons/8/85/BlackRock_wordmark.svg" /></a>
       </div>
       <div className="logos-slider inline-block space-x-[200px] p-[30px] animate-loop-scroll">
-        <img loading="lazy" src={mrbeast} />
-        <img loading="lazy" src={forbes} />
-        <img loading="lazy" src="https://upload.wikimedia.org/wikipedia/commons/8/85/BlackRock_wordmark.svg" />
+        <a href="https://youtu.be/raGL4DIh40s?si=ESJ61nwuLuqX71-f" target="_blank"><img loading="lazy" src={mrbeast} /></a>
+        <a href="https://www.forbes.com/billionaires/" target="_blank"><img loading="lazy" src={forbes} /></a>
+        <a href="https://www.reddit.com/r/GGdiscussion/comments/1g06rt7/can_someone_explain_to_me_what_blackrocks_evil/" target="_blank"><img loading="lazy" src="https://upload.wikimedia.org/wikipedia/commons/8/85/BlackRock_wordmark.svg" /></a>
       </div>
       <div className="logos-slider inline-block space-x-[200px] p-[30px] animate-loop-scroll">
-        <img loading="lazy" src={mrbeast} />
-        <img loading="lazy" src={forbes} />
-        <img loading="lazy" src="https://upload.wikimedia.org/wikipedia/commons/8/85/BlackRock_wordmark.svg" />
+        <a href="https://youtu.be/raGL4DIh40s?si=ESJ61nwuLuqX71-f" target="_blank"><img loading="lazy" src={mrbeast} /></a>
+        <a href="https://www.forbes.com/billionaires/" target="_blank"><img loading="lazy" src={forbes} /></a>
+        <a href="https://www.reddit.com/r/GGdiscussion/comments/1g06rt7/can_someone_explain_to_me_what_blackrocks_evil/" target="_blank"><img loading="lazy" src="https://upload.wikimedia.org/wikipedia/commons/8/85/BlackRock_wordmark.svg" /></a>
       </div>
     </div>
   )
