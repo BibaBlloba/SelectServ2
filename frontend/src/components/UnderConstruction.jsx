f073f57a1220fe1fa33f39a22f895dd9f399c3c4
Akeka_ <vladvaledol@gmail.com>
2025-01-23 23:46:48 +0300

    UnderConstruct AI

--- frontend/src/components/UnderConstruction.jsx
+++ frontend/src/components/UnderConstruction.jsx
@@ -3,11 +3,15 @@ import Construct from '../assets/constructuon.png'
 const UnderConstruction = ({ Chad }) => {
   return (
     <div className={`flex flex-col justify-center items-center h-screen
-      ${Chad ? "bg-[url(./assets/giga.jpg)] bg-cover" : "bg-white"}`}>
-      <h1 className={`text-[60px] font-bold
+      ${Chad ? "bg-[url(./assets/giga.jpg)] bg-cover" : "bg-[url(./assets/UnderConstruct4.jpeg)] bg-bottom bg-cover"}`}>
+      {Chad && (
+        <>
+          <h1 className={`text-[60px] font-bold
       ${Chad ? "text-white" : "text-black"}`
-      }>В разрабштке</h1>
-      <img src={Construct} alt="" />
+          }>В разрабштке</h1>
+          <img src={Construct} alt="" />
+        </>
+      )}
     </div>
   )
 }
