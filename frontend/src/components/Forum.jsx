7c7ada465a895da4c31befa5f7fc5ad2f0458289
Akeka_ <vladvaledol@gmail.com>
2025-01-28 16:04:18 +0300

    Redesign

--- frontend/src/components/Forum.jsx
+++ frontend/src/components/Forum.jsx
@@ -1,7 +1,8 @@
 import LoginContainer from "./LoginContainer";
 import { Modal } from "antd";
 import React, { useContext, useEffect, useState } from 'react'
-import { Input, Form, Button } from 'antd'
+import { Input, Form } from 'antd'
+import Button_dark from './Button_dark'
 import { UserContext } from '../context/UserContext';
 const { TextArea } = Input;
 
@@ -57,10 +58,10 @@ const Forum = () => {
   }, [])
 
   return (
-    <div className='min-h-screen bg-white flex flex-col p-[10px] gap-10 overflow-hidden'>
-      <div className='border-black border-2 h-[210px] flex flex-col p-[10px] px-[20px]'>
+    <div className='min-h-screen flex flex-col p-[10px] gap-10 overflow-hidden bg-[#171E29]'>
+      <div className='bg-[#1F2B3B] border-[#31435D] border-[1px] rounded-md h-[210px] flex flex-col p-[10px] px-[20px]'>
         <Form onFinish={sendMessage}>
-          <p>Оставте сообщение</p>
+          <p className="text-white mb-[5px]">Оставте сообщение</p>
           <Form.Item
             name="message"
             rules={[
@@ -72,19 +73,23 @@ const Forum = () => {
           >
             <TextArea rows={4} style={{
               resize: 'none',
+              backgroundColor: '#171E29',
+              color: 'white',
             }} />
           </Form.Item>
           <Form.Item>
-            <Button htmlType="submit" danger>Отправить</Button>
+            <Button_dark text={"Отправить"} />
           </Form.Item>
         </Form>
       </div>
       <div className='flex flex-col-reverse gap-5'>
         {data && data.map((message) => (
-          <div key={message.id} className='border-2 border-black min-h-28 flex flex-row'>
-            <div className='flex justify-center items-center border-2 border-black aspect-square m-2 h-[128px] w-[128px]'>{message.user_email}</div>
-            <div className="flex flex-col text-wrap">
+          <div key={message.id} className='border-[1px] border-[#31435D]  bg-[#1F2B3B] rounded-md  min-h-28 flex flex-row text-white'>
+            <div className="flex flex-col items-center mb-[15px]">
+              <div className='flex justify-center items-center border-[1px] bg-[#171E29] border-[#31435D] rounded-md aspect-square m-2 h-[128px] w-[128px]'>{message.user_email}</div>
               <p>{message.user_email}</p>
+            </div>
+            <div className="flex flex-col text-wrap p-[10px] gap-4">
               <p>{message.message}</p>
             </div>
           </div>
