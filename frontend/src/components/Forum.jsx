4b502a3c7c6959bdef94c9528b2f4a1a299f13b9
Akeka_ <vladvaledol@gmail.com>
2025-01-29 00:04:09 +0300

    Change text-size in forum

--- frontend/src/components/Forum.jsx
+++ frontend/src/components/Forum.jsx
@@ -134,9 +134,9 @@ const Forum = () => {
       <div className='flex flex-col-reverse gap-5'>
         {data && data.map((message) => (
           <div key={message.id} className='border-[1px] border-[#31435D]  bg-[#1F2B3B] rounded-md  min-h-28 flex flex-row text-white'>
-            <div className="flex flex-col items-center mb-[15px]">
-              <div className='flex justify-center items-center border-[1px] bg-[#171E29] border-[#31435D] rounded-md aspect-square m-2 sm:h-[128px] sm:w-[128px] h-[80px] w-[80px]'>{getStringBeforeCharacter(message.user_email, '@')}</div>
-              <p>{getStringBeforeCharacter(message.user_email, '@')}</p>
+            <div className="flex flex-col items-center mb-[5px] p-2 gap-2">
+              <div className='flex justify-center items-center border-[1px] bg-[#171E29] border-[#31435D] rounded-md aspect-square sm:h-[128px] sm:w-[128px] h-[80px] w-[80px] text-xl'>{getStringBeforeCharacter(message.user_email, '@')}</div>
+              <p className="text-pretty">{getStringBeforeCharacter(message.user_email, '@')}</p>
               {isSuper &&
                 <button className="mt-3 w-[100px] h-[35px] transition-colors delay-100 hover:border-red-600 hover:text-red-600  active:bg-red-900 border-solid border-[1px] rounded-md"
                   name={message.id}
