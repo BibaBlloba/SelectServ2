3d80a20c01063c83f05b5272f9d29883757860fa
Akeka_ <vladvaledol@gmail.com>
2025-01-28 15:51:18 +0300

    Add dark mode to dark button)

--- frontend/src/components/Button_dark.jsx
+++ frontend/src/components/Button_dark.jsx
@@ -1,10 +1,10 @@
 import React from 'react'
 
-const Button_dark = ({ className, onClick, href, text }) => {
+const Button_dark = ({ className, onClick, href, text, dark }) => {
   return (
     <>
-      <button className='border-solid border-[1px] rounded-md text-gray-300 border-gray-300 w-[100px] h-[35px]
-        transition-colors delay-100 hover:border-red-600 hover:text-red-600  active:bg-red-900'
+      <button className={`border-solid border-[1px] rounded-md ${dark ? "text-black" : "text-gray-300 "} ${dark ? "border-black" : "border-gray-300 "} w-[100px] h-[35px]
+        transition-colors delay-100 hover:border-red-600 hover:text-red-600  active:bg-red-900`}
         onClick={onClick}>{text}</button>
     </>
   )
