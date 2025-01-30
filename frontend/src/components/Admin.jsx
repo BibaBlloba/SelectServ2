7ed0ccf0c99681726db46124478f6820434eb969
Akeka_ <vladvaledol@gmail.com>
2025-01-30 12:04:09 +0300

    init

---
+++ frontend/src/components/Admin.jsx
@@ -0,0 +1,11 @@
+import Admin_LeftBar from "./Admin_LeftBar";
+
+const Admin = () => {
+  return (
+    <div className="h-screen bg-gray-800 flex flex-row">
+      <Admin_LeftBar />
+    </div>
+  );
+};
+
+export default Admin;
---
+++ frontend/src/components/Admin_LeftBar.jsx
@@ -0,0 +1,11 @@
+const Admin_LeftBar = () => {
+  return (
+    <div className="flex flex-col w-[200px]">
+      <p>aa</p>
+      <p>aa</p>
+      <p>aa</p>
+    </div>
+  );
+};
+
+export default Admin_LeftBar;
